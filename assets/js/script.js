b1b0564f9a6f8bb08b6cf76d1a217f9abc35c18a
sandy <sandy-sanders@msn.com>
2021-09-28 08:05:24 -0700

    submit several tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
-
   event.preventDefault();
-
   var taskNameInput = document.querySelector("input[name='task-name'").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
  // create list item
  var listItemEl = document.createElement("li");
  listItemEl.className = "task-item";
@@ -21,12 +21,8 @@ var createTaskHandler = function (event) {
 
  // add entire list item to list
  tasksToDoEl.appendChild(listItemEl);
-  var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  console.dir(taskNameInput);
-
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = taskNameInput;
+  
+  
   tasksToDoEl.appendChild(listItemEl);
 };
 
